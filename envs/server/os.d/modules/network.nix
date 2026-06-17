1f5c32d3b438470b01a6f6648e7cf9da87771983
Xilong Yang <contact@xilong.site>
2026-06-17 11:03:50 +0900

    static ip

--- envs/server/os.d/modules/network.nix
+++ envs/server/os.d/modules/network.nix
@@ -15,4 +15,10 @@
   };
 
   networking.firewall.allowedTCPPorts = [ 8000 ];
+  networking.interfaces.eth0.ipv4.addresses = [{
+    address = "10.5.27.10";
+    prefixLength = 24;
+  }];
+  networking.defaultGateway = "10.5.27.1";
+  networking.nameservers = ["1.1.1.1"];
 }
