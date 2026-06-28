f3b95138877eea66ab434ce29e2929dab310b3d6
Xilong Yang <contact@xilong.site>
2026-06-28 21:37:22 +0900

    fix: markdown error

--- shared/nvim/lua/plugins/aerial.lua
+++ shared/nvim/lua/plugins/aerial.lua
@@ -1,20 +1,21 @@
 return {
   {'stevearc/aerial.nvim',
-    opts = {},
+    opts = {
+      backends = {
+        ["_"] = { "treesitter", "lsp", "markdown", "asciidoc", "man" },
+        markdown = { "markdown" },
+      },
+      -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+      on_attach = function(bufnr)
+        -- Jump forwards/backwards with '{' and '}'
+        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+      end,
+    },
     -- Optional dependencies
     dependencies = {
        "nvim-treesitter/nvim-treesitter",
        "nvim-tree/nvim-web-devicons"
     },
-    config = function()
-        require("aerial").setup({
-          -- optionally use on_attach to set keymaps when aerial has attached to a buffer
-          on_attach = function(bufnr)
-            -- Jump forwards/backwards with '{' and '}'
-            vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-            vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-          end,
-        })
-    end,
   },
 }
